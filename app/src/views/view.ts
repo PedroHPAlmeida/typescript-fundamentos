f8ca0697260fb89dc382c6673e378017a88b72a8
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-08-02 20:38:35 -0300

    Decorators sem parâmetros

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -20,7 +20,7 @@ export abstract class View<T> {
 
     protected abstract template(model: T): string;
 
-    @inspect()
+    @inspect
     @logarTempoDeExecucao(true)
     public update(model: T): void {
         let template = this.template(model);
