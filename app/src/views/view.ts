1127104453b987241f5b703ac4d0b7fba5ef8178
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2023-08-02 20:47:42 -0300

    Mais um decorator

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -4,17 +4,13 @@ import { logarTempoDeExecucao } from "../decorators/logarTempoExecucao.js";
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
-    private escapar: boolean = false;
 
-    constructor(seletor: string, escapar?: boolean) {
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento as HTMLElement;
         } else {
             throw new Error(`Seletor ${seletor} não existe no DOM.`);
-        }
-        if (escapar) {
-            this.escapar = escapar;
         }
     }
 
@@ -24,9 +20,6 @@ export abstract class View<T> {
     @logarTempoDeExecucao(true)
     public update(model: T): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 
